ca2a522e2d78ce59110aa1cd2a6cd7ee04d2ef56
unknown <juankliabaras@gmail.com>
2022-03-01 23:41:04 -0300

    pagina web de videojuegos con JS

---
+++ index.js
@@ -0,0 +1,12 @@
+function main() {
+  const autor = {
+    nombre: "Juan",
+    apellido: "Kliabaras",
+    edad: 26,
+  };
+  console.log("titulo de mi pagina: ", document.title);
+  console.log("ancho de la pagina: ", window.innerWidth);
+  console.log("largo de la pagina: ", window.innerHeight);
+  console.log("autor: ", autor);
+}
+main();
